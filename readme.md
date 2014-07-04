16794316c4f203cec0e71a67cb5f0ca405fea36b
Jamie Hoover ⚔ <uipoet@users.noreply.github.com>
2014-07-03 17:56:04 -0700

    Initial commit. Includes working example with a few initial tests.

---
+++ readme.md
@@ -0,0 +1,52 @@
+# cssmod
+
+Casading Style Sheets, modified...
+
+## with:
+- modules, `@import` like commonjs `require()`
+- variables, like future CSS specification
+- color adjustment
+
+## without:
+- vendor prefixes
+
+## command line interface
+
+```bash
+npm install cssmod -g
+
+cssmod example build
+
+# modifies ./example/index.css (and any imported modules) into ./build/bundle.css
+```
+
+## application program interface
+
+```bash
+npm install cssmod --save-dev
+```
+
+```JavaScript
+var
+	cssmod = require('cssmod');
+
+cssmod({
+	input: 'example',
+	output: 'build'
+});
+```
+
+Resolving input/output file paths mimicks Node.js
+
+`input` option resolves in order:
+- PATH/FILENAME.css
+- PATH/index.css
+- ./index.css
+- error
+
+`output` option resolves in order:
+- PATH/FILENAME.css
+- PATH/bundle.css
+- ./build/bundle.css
+
+`output` will create directories and files as needed.
---
+++ tests/build.js
@@ -0,0 +1,59 @@
+var
+	cssmod = require('../lib/cssmod'),
+	fs = require('fs'),
+	path = require('path'),
+	test = require('tape');
+
+test('build', function (t) {
+  t.plan(1);
+
+	cssmod({
+		cwd: path.resolve(__dirname, '..'),
+		input: 'examples',
+		output: 'build',
+		sourcemap: true
+	}, function (error) {
+		if (error) {
+			t.fail(error);
+		} else {
+			var
+				output = path.resolve(__dirname, '../build/bundle.css');
+
+			if (fs.existsSync(output)) {
+				t.pass();
+			}
+		}
+	});
+});
+
+test('cleanup', function (t) {
+  t.plan(1);
+
+	cssmod({
+		cwd: path.resolve(__dirname, '..'),
+		input: 'examples',
+		output: 'build',
+		sourcemap: true
+	}, function (error) {
+		if (error) {
+			t.fail(error);
+		} else {
+			var
+				output = path.resolve(__dirname, '../build/bundle.css');
+
+			fs.unlink(output, function (error) {
+				if (error) {
+					t.fail(error);
+				} else {
+					fs.rmdir(path.dirname(output), function (error) {
+						if (error) {
+							t.fail(error);
+						} else {
+							t.pass();
+						}
+					});
+				}
+			});
+		}
+	});
+});
